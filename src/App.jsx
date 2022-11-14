21c58a5ae39b4fdd6effe237f03f5ef822ee0d7a
mahfuj890 <mahfujali490@gmail.com>
2022-11-14 12:16:35 +0600

    add route

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
-
-import './App.scss'
-import Home from './Pages/Home'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.scss";
+import Home from "./Pages/Home";
+import Search from "./Pages/Search";
 
 function App() {
- 
-
   return (
     <div className="App">
-    <Home />
-     
+    <BrowserRouter> 
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/search" element={<Search />} />
+      </Routes>
+      </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
